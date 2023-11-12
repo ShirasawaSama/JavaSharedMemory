8ac7cf14895dd16bdccbe762101a17d7c9661b62
Shirasawa <764798966@qq.com>
2023-11-12 16:04:54 +0800

    Fix bugs

--- include/jshm.h
+++ include/jshm.h
@@ -12,52 +12,56 @@
 #endif
 
 namespace jshm {
-	class shared_memory {
-	public:
-		[[nodiscard]] int size() const noexcept { return _size; }
-		[[nodiscard]] const char* name() const noexcept { return _name; }
-		
-		~shared_memory() {
+    class shared_memory {
+    public:
+        [[nodiscard]] int size() const noexcept { return _size; }
+        [[nodiscard]] const char* name() const noexcept { return _name; }
+        
+        ~shared_memory() {
 #ifdef _WIN32
-			UnmapViewOfFile(_pBuf);
-			CloseHandle(hMapFile);
+            UnmapViewOfFile(_pBuf);
+            CloseHandle(hMapFile);
 #elif __linux__ || __APPLE__
             if (_pBuf) munmap(_pBuf, (size_t) _size);
             if (_isCreate) shm_unlink(_name);
 #endif
-		}
+            delete[] _name;
+        }
 
-		[[nodiscard]] void* address() const noexcept { return _pBuf; }
+        [[nodiscard]] void* address() const noexcept { return _pBuf; }
 
         [[nodiscard]] static shared_memory* create(const char* name, int size) { return init(name, size, true); }
         [[nodiscard]] static shared_memory* open(const char* name, int size) { return init(name, size, false); }
 
-	private:
-		int _size;
-		const char* _name;
+    private:
+        int _size;
+        const char* _name;
 
 #ifdef _WIN32
-		HANDLE hMapFile;
-		LPTSTR _pBuf;
+        HANDLE hMapFile;
+        LPTSTR _pBuf;
 
-		shared_memory(HANDLE hMapFile, LPTSTR pBuf, int size, const char* name) : hMapFile(hMapFile), _pBuf(pBuf), _size(size), _name(name) { }
+        shared_memory(HANDLE hMapFile, LPTSTR pBuf, int size, const char* name) : hMapFile(hMapFile), _pBuf(pBuf), _size(size) {
 #elif __linux__ || __APPLE__
         void* _pBuf = nullptr;
         bool _isCreate;
-        shared_memory(void* pBuf, int size, const char* name, bool isCreate) : _size(size), _name(name), _pBuf(pBuf), _isCreate(isCreate) { }
+        shared_memory(void* pBuf, int size, const char* name, bool isCreate) : _size(size), _pBuf(pBuf), _isCreate(isCreate) {
 #endif
+            _name = new char[strlen(name) + 1];
+            strcpy((char*) _name, name);
+        }
 
-		static shared_memory* init(const char* name, int size, bool isCreate) {
+        static shared_memory* init(const char* name, int size, bool isCreate) {
 #ifdef _WIN32
-			auto hMapFile = isCreate ? CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE | SEC_COMMIT, 0, size, name)
-				: OpenFileMapping(SECTION_MAP_WRITE | SECTION_MAP_READ, FALSE, name);
-			if (hMapFile == NULL) return nullptr;
-			auto pBuf = (LPTSTR)MapViewOfFile(hMapFile, SECTION_MAP_WRITE | SECTION_MAP_READ, 0, 0, size);
-			if (pBuf == NULL) {
-				CloseHandle(hMapFile);
-				throw nullptr;
-			}
-			return new shared_memory(hMapFile, pBuf, size, name);
+            auto hMapFile = isCreate ? CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE | SEC_COMMIT, 0, size, name)
+                : OpenFileMapping(SECTION_MAP_WRITE | SECTION_MAP_READ, FALSE, name);
+            if (hMapFile == NULL) return nullptr;
+            auto pBuf = (LPTSTR)MapViewOfFile(hMapFile, SECTION_MAP_WRITE | SECTION_MAP_READ, 0, 0, size);
+            if (pBuf == NULL) {
+                CloseHandle(hMapFile);
+                throw nullptr;
+            }
+            return new shared_memory(hMapFile, pBuf, size, name);
 #elif __linux__ || __APPLE__
             auto mode = O_RDWR;
             if (isCreate) mode |= O_CREAT | O_EXCL;
@@ -74,8 +78,8 @@ namespace jshm {
             }
             return new shared_memory(pBuf, size, name, isCreate);
 #endif
-		}
-	};
+        }
+    };
 }
 
 #endif
